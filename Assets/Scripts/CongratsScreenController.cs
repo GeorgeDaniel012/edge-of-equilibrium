2afff98b9b21e6a10e039e9599fb32cea40b0515
Silaghi Mara <123313785+MaraSilaghi@users.noreply.github.com>
2025-01-11 17:48:34 +0200

    congrats screen added

---
+++ Assets/Scripts/CongratsScreenController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CongratsScreenController : MonoBehaviour
+{
+   
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
